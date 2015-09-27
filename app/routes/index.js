3a16266eb0a3b485febd74fcdda7972960cefa59
Christian <christian.lei1@gmail.com>
2015-09-27 13:52:37 -0700

    sources db

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,31 @@
 import Ember from 'ember';
 
+// var hoards = [{
+//   id: 1,
+//   name: "Veruca Salt",
+//   city: "San Francisco",
+//   type: "Estate",
+//   bedrooms: 15,
+//   image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
+// }, {
+//   id: 2,
+//   name: "Mike TV",
+//   city: "Seattle",
+//   type: "Condo",
+//   bedrooms: 1,
+//   image: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg"
+// }, {
+//   id: 3,
+//   name: "Violet Beauregarde",
+//   city: "Portland",
+//   type: "Apartment",
+//   bedrooms: 3,
+//   image: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg"
+// }];
+
 export default Ember.Route.extend({
   model() {
+    // return hoards;
     return this.store.findAll('hoard');
   },
 });
