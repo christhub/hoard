1483f7989d182fb0308fc4c4ab3bd75fd86e312c
Christian <christian.lei1@gmail.com>
2015-09-27 21:17:14 -0700

    everything but comments

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,4 +5,18 @@ const { inject } = Ember;
 
 export default FirebaseAdapter.extend({
   firebase: inject.service(),
+  // actions: {
+  //   hoardFormControlPlus () {
+  //     this.set('hoardOpen', true);
+  //   },
+  //
+  //   hoardFormControlDown () {
+  //     this.set('hoardOpen', false);
+  //   },
+  //   save3(params) {
+  //     var newHoard = this.store.createRecord('hoard',params);
+  //     newHoard.save();
+  //     this.transitionTo('index');
+  //   },
+  // }
 });
